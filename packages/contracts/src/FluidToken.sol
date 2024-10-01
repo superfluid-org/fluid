c1154e87502fa8cd1102dcf818dc1e5c7a8c5d6a
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-10-01 14:47:42 +0200

    feat: add FLUID token contract draft

---
+++ packages/contracts/src/FluidToken.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FluidToken is ERC20 {
+    constructor() ERC20("FLUID Token", "FLUID") {}
+}
