baa4d445f7c08105df7d40e14cd182168e0b7338
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-10-11 16:44:25 +0200

    feat: draft deploy script

---
+++ packages/contracts/script/Deploy.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { Script } from "forge-std/Script.sol";
+import {
+    ISuperfluid,
+    ISuperToken
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import { EPProgramManager, IEPProgramManager } from "../src/EPProgramManager.sol";
+import { FluidLocker } from "../src/FluidLocker.sol";
+import { FluidLockerFactory } from "../src/FluidLockerFactory.sol";
+import { Fontaine } from "../src/Fontaine.sol";
+import { PenaltyManager, IPenaltyManager } from "../src/PenaltyManager.sol";
+
+// forge script script/Deploy.s.sol:DeployScript --rpc-url $RPC_URL --broadcast --verify -vvvv
+contract DeployScript is Script {
+    function setUp() public { }
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        ISuperToken fluid = ISuperToken(vm.envAddress("FLUID_ADDRESS"));
+
+        // Deploy Ecosystem Partner Program Manager
+        EPProgramManager programManager = new EPProgramManager();
+
+        // Deploy Penalty Manager
+        PenaltyManager penaltyManager = new PenaltyManager(msg.sender, fluid);
+
+        // Read the newly created GDA Penalty Pool address
+        ISuperfluidPool penaltyDrainingPool = penaltyManager.PENALTY_DRAINING_POOL();
+
+        // Deploy the Fluid Locker Implementation contract
+        FluidLocker fluidLockerImpl =
+            new FluidLocker(fluid, penaltyDrainingPool, IEPProgramManager(address(programManager)));
+
+        // Deploy the Fontaine Implementation contract
+        Fontaine fontaineImpl = new Fontaine(fluid, penaltyDrainingPool);
+
+        // Deploy the Fluid Locker Factory contract
+        FluidLockerFactory lockerFactory = new FluidLockerFactory(
+            address(fluidLockerImpl), address(fontaineImpl), IPenaltyManager(address(penaltyManager))
+        );
+
+        // Sets the FluidLockerFactory address in the PenaltyManager
+        penaltyManager.setLockerFactory(address(lockerFactory));
+    }
+}
