53fc7784f95647567b25467f605a02b6abd2b716
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-10-15 09:56:37 +0200

    feat: script to extract clean ABI

---
+++ packages/contracts/extract-abi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+chains=(84532)
+
+# Loop over each chainID
+for chain in ${chains[@]}; do
+
+  output_folder="abi/"
+  input_file="broadcast/Deploy.s.sol/$chain/run-latest.json"
+
+  # Create the output folder if it does not exist
+  mkdir -p "$output_folder"
+
+  # Extract contract names and addresses using jq
+  contracts=$(jq -c '.transactions[] | {name: .contractName}' "$input_file")
+
+  # Loop over each contract and create a JSON file with the contract address
+  for contract in $contracts; do
+    name=$(echo "$contract" | jq -r '.name')
+    contract_json_file="out/$name.sol/$name.json"
+
+    abi=$(jq -r '.abi' "$contract_json_file")
+
+    echo "$abi" > "$output_folder/${name}.json"
+  done
+done
