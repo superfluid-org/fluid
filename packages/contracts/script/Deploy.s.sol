980898c8786b76a0be11e14a3f59d63deedcff3c
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-10-15 14:25:16 +0200

    update: deploy script review

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 import { Script } from "forge-std/Script.sol";
 import {
@@ -14,38 +14,54 @@ import { FluidLockerFactory } from "../src/FluidLockerFactory.sol";
 import { Fontaine } from "../src/Fontaine.sol";
 import { PenaltyManager, IPenaltyManager } from "../src/PenaltyManager.sol";
 
+function deployAll(ISuperToken fluid, address owner)
+    returns (
+        address programManagerAddress,
+        address penaltyManagerAddress,
+        address lockerFactoryAddress,
+        address lockerLogicAddress,
+        address fontaineLogicAddress
+    )
+{
+    // Deploy Ecosystem Partner Program Manager
+    EPProgramManager programManager = new EPProgramManager();
+    programManagerAddress = address(programManager);
+
+    // Deploy Penalty Manager
+    PenaltyManager penaltyManager = new PenaltyManager(owner, fluid);
+    penaltyManagerAddress = address(penaltyManager);
+
+    // Read the newly created GDA Penalty Pool address
+    ISuperfluidPool penaltyDrainingPool = penaltyManager.PENALTY_DRAINING_POOL();
+
+    // Deploy the Fluid Locker Implementation contract
+    FluidLocker fluidLockerImpl =
+        new FluidLocker(fluid, penaltyDrainingPool, IEPProgramManager(address(programManager)));
+    lockerLogicAddress = address(fluidLockerImpl);
+
+    // Deploy the Fontaine Implementation contract
+    Fontaine fontaineImpl = new Fontaine(fluid, penaltyDrainingPool);
+    fontaineLogicAddress = address(fontaineImpl);
+
+    // Deploy the Fluid Locker Factory contract
+    FluidLockerFactory lockerFactory = new FluidLockerFactory(
+        address(fluidLockerImpl), address(fontaineImpl), IPenaltyManager(address(penaltyManager))
+    );
+    lockerFactoryAddress = address(lockerFactory);
+
+    // Sets the FluidLockerFactory address in the PenaltyManager
+    penaltyManager.setLockerFactory(address(lockerFactory));
+}
+
 // forge script script/Deploy.s.sol:DeployScript --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvvv
 contract DeployScript is Script {
     function setUp() public { }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-
         ISuperToken fluid = ISuperToken(vm.envAddress("FLUID_ADDRESS"));
 
-        // Deploy Ecosystem Partner Program Manager
-        EPProgramManager programManager = new EPProgramManager();
-
-        // Deploy Penalty Manager
-        PenaltyManager penaltyManager = new PenaltyManager(vm.addr(deployerPrivateKey), fluid);
-
-        // Read the newly created GDA Penalty Pool address
-        ISuperfluidPool penaltyDrainingPool = penaltyManager.PENALTY_DRAINING_POOL();
-
-        // Deploy the Fluid Locker Implementation contract
-        FluidLocker fluidLockerImpl =
-            new FluidLocker(fluid, penaltyDrainingPool, IEPProgramManager(address(programManager)));
-
-        // Deploy the Fontaine Implementation contract
-        Fontaine fontaineImpl = new Fontaine(fluid, penaltyDrainingPool);
-
-        // Deploy the Fluid Locker Factory contract
-        FluidLockerFactory lockerFactory = new FluidLockerFactory(
-            address(fluidLockerImpl), address(fontaineImpl), IPenaltyManager(address(penaltyManager))
-        );
-
-        // Sets the FluidLockerFactory address in the PenaltyManager
-        penaltyManager.setLockerFactory(address(lockerFactory));
+        vm.startBroadcast(deployerPrivateKey);
+        deployAll(fluid, vm.addr(deployerPrivateKey));
     }
 }
