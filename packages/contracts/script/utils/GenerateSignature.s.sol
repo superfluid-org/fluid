ad234ccc92beb28279e4f17d5395068ad61bd771
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-10-17 16:24:45 +0200

    feat: added smol utils script to generate signature for frontend testing

---
+++ packages/contracts/script/utils/GenerateSignature.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+using ECDSA for bytes32;
+
+// forge script script/utils/GenerateSignature.s.sol:GenerateSignature -vvvv
+contract GenerateSignature is Script {
+    uint256 public signerPrivateKey;
+
+    function setUp() public {
+        signerPrivateKey = vm.envUint("SIGNER_PRIVATE_KEY");
+    }
+
+    function run() public {
+        uint128 unitsToGrant = 5;
+        uint96 programId = 0;
+        uint256 nonce = 1;
+
+        _generateSignature(unitsToGrant, programId, nonce);
+    }
+
+    function _generateSignature(uint128 _unitsToGrant, uint96 _programId, uint256 _nonce) internal {
+        bytes32 message =
+            keccak256(abi.encodePacked(0x37dB1380669155d6080c04a5e6Db029E306cD964, _unitsToGrant, _programId, _nonce));
+
+        bytes32 digest = message.toEthSignedMessageHash();
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, digest);
+        bytes memory validSignature = abi.encodePacked(r, s, v);
+
+        console2.log("Message : ");
+        console2.logBytes32(message);
+
+        console2.log("Signed Message : ");
+        console2.logBytes32(digest);
+
+        console2.log("Signer Address : ");
+        console2.log(vm.addr(signerPrivateKey));
+
+        console2.log("Signature : ");
+        console2.logBytes(validSignature);
+
+        console2.log("Signature Length: ");
+        console2.log(validSignature.length);
+    }
+}
