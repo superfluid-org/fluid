7309254dbeb8aa3e9ec004d2f7052d51db8dbaa4
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-11-07 15:11:34 +0100

    fix: update script

--- packages/tasks/show-git-rev.sh
+++ packages/tasks/show-git-rev.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 {
-    echo -n "$(git rev-parse --short HEAD)"
-    [ -n "$(git status --porcelain)" ] && echo -n " (dirty)"
+    echo "$(git rev-parse --short HEAD)"
+    [ -n "$(git status --porcelain)" ] && echo " (dirty)"
 } | {
     if [ "$1" == forge_ffi_mode ]; then
         xxd -p
