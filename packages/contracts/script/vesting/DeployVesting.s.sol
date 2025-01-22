8b27fe79cc3079b849bd7d2382608f51a9ee5287
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-01-22 15:07:16 +0100

    feat: added deploy script for vesting contracts

--- packages/contracts/script/vesting/DeployVesting.s.sol
+++ packages/contracts/script/vesting/DeployVesting.s.sol
@@ -3,13 +3,43 @@ pragma solidity ^0.8.23;
 
 import { Script, console2 } from "forge-std/Script.sol";
 
-import {
-    ISuperfluid,
-    ISuperfluidPool,
-    ISuperToken
-} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { IVestingSchedulerV2 } from
+    "@superfluid-finance/automation-contracts/scheduler/contracts/interface/IVestingSchedulerV2.sol";
 
-// forge script script/DeployVesting.s.sol:DeployVestingScript --ffi --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvvv
+import { SupVestingFactory } from "src/vesting/SupVestingFactory.sol";
+import { SupVesting } from "src/vesting/SupVesting.sol";
+
+function _deployVestingFactory(address vestingScheduler, address supToken, address treasury, address governor)
+    returns (address supVestingFactoryAddress)
+{
+    SupVestingFactory supVestingFactory =
+        new SupVestingFactory(IVestingSchedulerV2(vestingScheduler), ISuperToken(supToken), treasury, governor);
+    supVestingFactoryAddress = address(supVestingFactory);
+}
+
+function _deployDummyVesting(address vestingScheduler, address supToken, address governor)
+    returns (address supVestingAddress)
+{
+    SupVesting supVesting = new SupVesting(
+        IVestingSchedulerV2(vestingScheduler),
+        ISuperToken(supToken),
+        governor,
+        1 ether,
+        10 days,
+        uint32(block.timestamp + 1 days),
+        0 days
+    );
+    supVestingAddress = address(supVesting);
+}
+
+/*
+FLUID_ADDRESS=0xFd62b398DD8a233ad37156690631fb9515059d6A \
+VESTING_SCHEDULER_ADDRESS=0xF8c4eB09b51DD85cE315f76132F17f8FA5073e54 \
+GOVERNOR_ADDRESS=0xe7143e87661418DEA122941e01Fdb3f9Acfd02aB \
+TREASURY_ADDRESS=0xe7143e87661418DEA122941e01Fdb3f9Acfd02aB \
+forge script script/vesting/DeployVesting.s.sol:DeployVestingScript --ffi --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvv --etherscan-api-key $BASESCAN_API_KEY
+*/
 contract DeployVestingScript is Script {
     function setUp() public { }
 
@@ -18,7 +48,6 @@ contract DeployVestingScript is Script {
 
         // Deployer settings
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
 
         // Deployment parameters
         address vestingScheduler = vm.envAddress("VESTING_SCHEDULER_ADDRESS");
@@ -28,7 +57,9 @@ contract DeployVestingScript is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        SupVestingFactory supVestingFactory = new SupVestingFactory(vestingScheduler, supToken, treasury, governor);
+        address supVestingFactory = _deployVestingFactory(vestingScheduler, supToken, treasury, governor);
+        address dummySupVesting = _deployDummyVesting(vestingScheduler, supToken, governor);
+        console2.log("SupVestingFactory deployed at: ", supVestingFactory);
     }
 
     function _showGitRevision() internal {
