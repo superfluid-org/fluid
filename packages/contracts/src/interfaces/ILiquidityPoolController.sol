dd51cbf1ff07a7886a315ea44679703d5608279a
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-04-08 09:29:30 +0200

    feat: add liquidity pool controller contract

---
+++ packages/contracts/src/interfaces/ILiquidityPoolController.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+
+//                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@                        @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@                        @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@                        @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@                        @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@                        @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            @@@@@@@@@@@@
+//                   @@@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+pragma solidity ^0.8.23;
+
+/**
+ * @title Liquidity Pool Controller Contract Interface
+ * @author Superfluid
+ * @notice Contract responsible for administrating the Uniswap V3 Liquidity Pools available for the Lockers
+ *
+ */
+interface ILiquidityPoolController {
+    //      ______                 __
+    //     / ____/   _____  ____  / /______
+    //    / __/ | | / / _ \/ __ \/ __/ ___/
+    //   / /___ | |/ /  __/ / / / /_(__  )
+    //  /_____/ |___/\___/_/ /_/\__/____/
+
+    /// @notice Event emitted when a Liquidity Pool is whitelisted
+    event LiquidityPoolWhitelisted(uint256 poolId, address liquidityPool);
+
+    /// @notice Event emitted when a Liquidity Pool is blacklisted
+    event LiquidityPoolBlacklisted(uint256 poolId);
+
+    //      ______     __                        __   ______                 __  _
+    //     / ____/  __/ /____  _________  ____ _/ /  / ____/_  ______  _____/ /_(_)___  ____  _____
+    //    / __/ | |/_/ __/ _ \/ ___/ __ \/ __ `/ /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //   / /____>  </ /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    //  /_____/_/|_|\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice Whitelist a Liquidity Pool
+     * @dev Only the contract owner can perform this operation
+     * @param liquidityPool Liquidity Pool contract address to be whitelisted
+     */
+    function whitelistLiquidityPool(address liquidityPool) external;
+
+    /**
+     * @notice Blacklist a Liquidity Pool
+     * @dev Only the contract owner can perform this operation
+     * @param poolId Liquidity Pool id to be blacklisted
+     */
+    function blacklistLiquidityPool(uint256 poolId) external;
+
+    /**
+     * @notice Upgrade this proxy logic
+     * @dev Only the owner address can perform this operation
+     * @param newImplementation new logic contract address
+     * @param data calldata for potential initializer
+     */
+    function upgradeTo(address newImplementation, bytes calldata data) external;
+
+    //   _    ___                 ______                 __  _
+    //  | |  / (_)__ _      __   / ____/_  ______  _____/ /_(_)___  ____  _____
+    //  | | / / / _ \ | /| / /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  | |/ / /  __/ |/ |/ /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    //  |___/_/\___/|__/|__/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice Get a Liquidity Pool
+     * @param poolId Liquidity Pool id to be retrieved
+     * @return liquidityPool Liquidity Pool contract address
+     */
+    function getLiquidityPool(uint256 poolId) external view returns (address liquidityPool);
+}
